27b0fb45b0f3f74a4cf84f2ece5ffab4ad27c3c0
John Tarleton Moore <tarletonmoore@gmail.com>
2022-05-22 13:28:44 -0400

    commit tree f4c0860f6d53e23389372bfd3bc990fda8d5ea33

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,15 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import * as RMFunctions from "./parks/RockyMountain"
+
+console.log(RMFunctions.trees)
+
+RMFunctions.wildlife()
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
 }
+
+export default ColoradoStateParks
